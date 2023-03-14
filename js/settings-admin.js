04999d8e98b7cf67ab0857a6cf63a3a5cef19436
Micke Nordin <kano@sunet.se>
2023-03-14 16:52:14 +0100

    Add js for saving

---
+++ js/settings-admin.js
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: Enrique Pérez Arnaud <eperez@emergya.com>, Mikael Nordin <kano@sunet.se>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+$(document).ready(function () {
+
+	$('#jupyter_submit').on('click', function (event) {
+		event.preventDefault();
+		OC.msg.startSaving('#jupyterSettings .msg');
+
+		var app = "jupyter"
+		var url = $("#jupyter_url");
+		var urlValue = url.val();
+		if (urlValue.endsWith("/")) {
+			urlValue = urlValue.slice(0, -1);
+			url.val(urlValue);
+		}
+		OC.AppConfig.setValue(app, url.attr('name'), url.val());
+
+		OC.msg.finishedSaving('#jupyterSettings .msg', { status: 'success', data: { message: t('jupyter', 'Saved.') } });
+	});
+
+	$('.section .icon-info').tipsy({ gravity: 'w' });
+});
+
