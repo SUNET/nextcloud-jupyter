1d78055e4699f64c4b202d930c7e0f8e94ed30b2
Micke Nordin <kano@sunet.se>
2023-03-14 18:29:31 +0100

    Update url service

--- lib/Service/UrlService.php
+++ lib/Service/UrlService.php
@@ -8,58 +8,37 @@ use \OCP\IConfig;
 
 class UrlService
 {
-    private $appName;
-    private $cloudUrlKey;
-    private $config;
+  private $appName;
+  private $cloudUrlKey;
+  private $config;
 
-    public function __construct($AppName, IConfig $config)
-    {
-        $this->config = $config;
-        $this->appName = $AppName;
-        $this->cloudUrlKey = "cloudURL";
-    }
+  public function __construct($AppName, IConfig $config)
+  {
+    $this->config = $config;
+    $this->appName = $AppName;
+    $this->cloudUrlKey = "jupiterURL";
+  }
 
-    public function getURL()
-    {
-        return $this->config->getAppValue($this->appName, $this->cloudUrlKey);
-    }
+  public function getURL()
+  {
+    return $this->config->getAppValue($this->appName, $this->cloudUrlKey);
+  }
 
-    public function setURL($value)
-    {
-        $this->config->setAppValue($this->appName, $this->cloudUrlKey, $value);
-        return $this->getURL();
-    }
+  public function setURL($value)
+  {
+    $this->config->setAppValue($this->appName, $this->cloudUrlKey, $value);
+    return $this->getURL();
+  }
 
-    public function getOverview()
-    {
-        return [
-            $this->cloudUrlKey => $this->getURL()
-        ];
-    }
+  public function getOverview()
+  {
+    return [
+      $this->cloudUrlKey => $this->getURL()
+    ];
+  }
 
-    public function getCloudUrlKey()
-    {
-        return $this->cloudUrlKey;
-    }
-
-    public function getPortURL()
-    {
-        return $this->getURL() . "/port-service";
-    }
-
-    public function getExporterURL()
-    {
-        return $this->getURL() . "/exporter";
-    }
-
-    public function getMetadataURL()
-    {
-        return $this->getURL() . "/metadata";
-    }
-
-    public function getResearchURL()
-    {
-        return $this->getURL() . "/research";
-    }
+  public function getCloudUrlKey()
+  {
+    return $this->cloudUrlKey;
+  }
 }
-
