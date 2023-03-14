8e39657632ac89089c3cb1ff9503a0245f986afd
Micke Nordin <kano@sunet.se>
2023-03-14 17:26:45 +0100

    Alert debug

--- js/settings-admin.js
+++ js/settings-admin.js
@@ -1,18 +1,19 @@
 // SPDX-FileCopyrightText: Enrique Pérez Arnaud <eperez@emergya.com>, Mikael Nordin <kano@sunet.se>
 // SPDX-License-Identifier: AGPL-3.0-or-later
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$('#jupyter_submit').on('click', function (event) {
-		event.preventDefault();
-		OC.msg.startSaving('#jupyterSettings .msg');
+  $('#jupyter_submit').on('click', function(event) {
+    event.preventDefault();
+    OC.msg.startSaving('#jupyterSettings .msg');
 
-		var app = "jupyter"
+    var app = "jupyter"
     let searchParams = new URLSearchParams(window.location.search);
-    var urlValue = searchParams.get('jupyter_url')
+    var urlValue = searchParams.get('jupyter_url');
+    alert(urlValue);
     OC.AppConfig.setValue(app, 'jupyterURL', urlValue);
     OC.msg.finishedSaving('#jupyterSettings .msg', { status: 'success', data: { message: t('jupyter', 'Saved.') } });
-	});
+  });
 
-	//$('.section .icon-info').tipsy({ gravity: 'w' });
+  //$('.section .icon-info').tipsy({ gravity: 'w' });
 });
 
